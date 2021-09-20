e10e7e4ccfa7f14bb58ffbd990bb7c9aa82044b6
lorenzorru0 <lorenzoorru999@gmail.com>
2021-09-20 15:50:24 +0200

    Add graphic part

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 const app = new Vue ({
     el: "#root",
     data: {
-
+        todos: [
+            {
+                text: "Fare i compiti",
+                done: false
+            },
+            {
+                text: "Fare la spesa",
+                done: false
+            },
+            {
+                text: "Fare il bucato",
+                done: false
+            }
+        ]
     },
     methods: {
-        
+
     }
 });
