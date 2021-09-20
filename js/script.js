ef71fb0ffd37a7c2b1b231ec16dad61cc59e7a85
lorenzorru0 <lorenzoorru999@gmail.com>
2021-09-20 16:57:05 +0200

    Add al the exercise

--- js/script.js
+++ js/script.js
@@ -14,9 +14,26 @@ const app = new Vue ({
                 text: "Fare il bucato",
                 done: false
             }
-        ]
+        ],
+        newTodo: ""
     },
     methods: {
-
+        addTodo: function() {
+            let todoTest = {
+                text: "",
+                done: false
+            }
+            if ( this.newTodo != "" ) {
+                todoTest.text = this.newTodo;
+                this.todos.push(todoTest);
+                this.newTodo = "";
+            }
+        },
+        removeTodo: function(todoIndex) {
+            this.todos.splice(todoIndex, 1);
+        },
+        doneTodo: function (todoIndex) {
+            this.todos[todoIndex].done = true;
+        }
     }
 });
